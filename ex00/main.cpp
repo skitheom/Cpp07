cc5a2190eee1bae1d37de4addde61b0a93a51041
skitheom <SahoKitahara@my.uopeople.edu>
2025-01-18 20:38:26 +0900

    ok

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,24 +6,30 @@
 /*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/14 16:01:47 by sakitaha          #+#    #+#             */
-/*   Updated: 2025/01/14 16:35:41 by sakitaha         ###   ########.fr       */
+/*   Updated: 2025/01/18 20:38:15 by sakitaha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "whatever.hpp"
 #include <iostream>
 
 int main(void) {
+
   int a = 2;
   int b = 3;
+
   ::swap(a, b);
   std::cout << "a = " << a << ", b = " << b << std::endl;
   std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
   std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+
   std::string c = "chaine1";
   std::string d = "chaine2";
+
   ::swap(c, d);
   std::cout << "c = " << c << ", d = " << d << std::endl;
   std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
   std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+
   return 0;
 }
---
+++ ex00/whatever.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakitaha <sakitaha@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/18 13:16:02 by sakitaha          #+#    #+#             */
+/*   Updated: 2025/01/18 20:38:19 by sakitaha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+#define WHATEVER_HPP
+
+template <typename T> void swap(T &a, T &b) {
+  T temp = a;
+  a = b;
+  b = temp;
+}
+
+template <typename T> T &min(T &a, T &b) { return (a < b ? a : b); }
+
+template <typename T> const T &min(const T &a, const T &b) {
+  return (a < b ? a : b);
+}
+
+template <typename T> T &max(T &a, T &b) { return (a > b ? a : b); }
+
+template <typename T> const T &max(const T &a, const T &b) {
+  return (a > b ? a : b);
+}
+
+#endif // WHATEVER_HPP
