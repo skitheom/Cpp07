4edc93a13390b01a7620cc1b1f7a7ec8bfaff3e9
skitheom <SahoKitahara@my.uopeople.edu>
2025-02-26 18:20:13 +0900

    fix: return the 2nd one if equal

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -19,13 +19,13 @@ template <typename T> void swap(T &a, T &b) {
   b = temp;
 }
 
-template <typename T> T &min(T &a, T &b) { return (a <= b ? a : b); }
+template <typename T> T &min(T &a, T &b) { return (a < b ? a : b); }
 
 template <typename T> const T &min(const T &a, const T &b) {
   return (a <= b ? a : b);
 }
 
-template <typename T> T &max(T &a, T &b) { return (a >= b ? a : b); }
+template <typename T> T &max(T &a, T &b) { return (a > b ? a : b); }
 
 template <typename T> const T &max(const T &a, const T &b) {
   return (a >= b ? a : b);
